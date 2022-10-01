eecd5b9a60a05dec0ea8057a5a74fc00445ac026
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-01 17:04:01 -0300

    first commit

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -4,12 +4,10 @@ import { Div } from '../../components/Div/div';
 export default function Registration() {
     
     return (
-        <> 
-            
+        <>             
         <Div/>
         <div className="bg-greenDark h-16 w-16 flex justify-center">
-            <h1 className="italic">oi, tudo bom?</h1>
-            
+            <h1 className="italic">oi, tudo bom?</h1>            
         </div>
         </>
     );
