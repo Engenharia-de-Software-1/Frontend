ac368dfb94d2dd2d04789ba5c49c0c8a043e041d
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-01 16:58:03 -0300

    first commit

---
+++ src/pages/registration/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Div } from '../../components/Div/div';
+
+export default function Registration() {
+    
+    return (
+        <> 
+            
+        <Div/>
+        <div className="bg-greenDark h-16 w-16 flex justify-center">
+            <h1 className="italic">oi, tudo bom?</h1>
+            
+        </div>
+        </>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "greenDark": "#429453"
+      },
+      fontSize: {
+        "953": "59.5625rem",
+        "559": "34.9375rem"
+      },
+      width: {
+
+      },
+      height: {
+
+      },
+    },
+  },
+  plugins: [],
+}
