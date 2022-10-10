9010370604aa51e23477837e4d0f1b829bd751d9
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-09 22:01:55 -0300

    para deletar branch

--- src/pages/minhaConta/index.tsx
+++ src/pages/minhaConta/index.tsx
@@ -15,6 +15,7 @@ export default function ProfileAdmin() {
         setButtonChangePassword(false);
         setButtonDelete(false);
     }
+    
     function goChangePassword() {
         setButtonEdit(false);
         setButtonChangePassword(true);
