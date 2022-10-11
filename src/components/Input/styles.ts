27e3ab4a898bdab2227ad7bf4be2997bbb9f0749
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-11 20:21:52 -0300

    refactor: registration starup

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,4 @@
 // Estilização do componente Div
-export const textStyle = 'italic text-sm';
 export const mainStyle = 'w-full h-10 bg-optionWhite text-sm rounded-[5px]';
 export const inputStyle = 'w-full h-10 bg-optionWhite text-sm px-4 pb-2 rounded-lg outline-none';
-export const legendStyle = 'ml-4 text-grayText';
+export const selectStyle = 'ml-4 text-grayText';
