18215191b14bbf9656a9e6bd6c868a482105f39c
Nuisigor <dexchanel@gmail.com>
2022-10-13 21:35:10 -0300

    axios in startup

--- src/pages/cadastroStartup/index.tsx
+++ src/pages/cadastroStartup/index.tsx
@@ -1,4 +1,4 @@
-import router from 'next/router';
+import router, { useRouter } from 'next/router';
 import React, { useCallback, useState } from 'react';
 import CityValues from '../../contents/city';
 import { Button } from '../../components/Button';
@@ -6,18 +6,41 @@ import { Input } from '../../components/Input';
 import { Select } from '../../components/Select';
 import { Stack } from '../../components/Stack';
 import { divGeneral, textTitle } from './styles';
+import axiosInstance from '../axiosInstance';
+
+interface ICadastroStartup{
+    startupName: string;
+    cnpj: string;
+    phone: string;
+    employees: number;
+    state: string;
+    city: string;
+}
+
 
 export default function Registration() {    
-    const [state, setState] = useState("");
-    const [city, setCity] = useState("");   
+    const [cadastro,setCadastro] = useState<ICadastroStartup>({} as ICadastroStartup);
+    
+    const router = useRouter();
+    const { userId } = router.query;
 
-    const handleUF = useCallback((state:string) => {
-        setState(state)
-    }, [])
-
-    const handleCity = useCallback((city:string) => {
-        setCity(city)
-    }, [])
+    const handleChange = (e: any) => {
+        setCadastro({
+          ...cadastro,
+          [e.target.name]: e.target.value //edit
+        });
+    };
+        
+    const handleSubmit = async (e:any) =>{
+        e.preventDefault();
+        try {
+            await axiosInstance.put(`/startup/${userId}`, cadastro)
+            router.push('/minhaContaStartup')
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
     
     function goBack() {
         router.push('/cadastro') 
@@ -36,25 +59,25 @@ export default function Registration() {
 
                     <div className="w-462 mt-5">
                         <Input haslabel label='Nome do representante' placeholder='Ex: José da Silva'/>
-                        <Input haslabel label='Nome da startup' placeholder='Ex: Doe sangue' top='mt-10'/>
-                        <Input haslabel label='Némero de celular' placeholder='(00) 0 0000-0000' top='mt-10'/>
-                        <Input haslabel label='CNPJ' placeholder='00000000000000' top='mt-10'/>
-                        <Input haslabel label='Quantidade de pessoas na startup' placeholder='0' type='number' min='0' top='mt-10'/>                
+                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.startupName} label='Nome da startup' placeholder='Ex: Doe sangue' top='mt-10'/>
+                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.phone} label='Número de celular' placeholder='(00) 0 0000-0000' top='mt-10'/>
+                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.cnpj} label='CNPJ' placeholder='00000000000000' top='mt-10'/>
+                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.employees} label='Quantidade de pessoas na startup' placeholder='0' type='number' min='0' top='mt-10'/>                
 
                         <div className='flex space-x-10'>
-                            <Select onChange = {(e) => handleUF(e.target.value)} value = {state} 
-                                haslabel label='Estado' top='mt-10'
+                            <Select onChange = {(e) => handleChange(e)} value = {cadastro.state} 
+                                haslabel label='Estado' top='mt-10' name='state'
                                 >
                                 <option key = 'init'>Selecione o Estado</option>
                                 {CityValues.estados.map((uf, index) => (
                                     <option key ={index.toString()} value = {uf.sigla}>{uf.nome}</option>
                                 ))}
                             </Select>
-                            <Select onChange = {(e) => handleCity(e.target.value)} value = {city} 
+                            <Select onChange = {(e) => handleChange(e)} value = {cadastro.city} 
                                 haslabel label='Cidade' top='mt-10'
                                 >
                                 <option key = 'init'>Selecione a cidade</option>
-                                {CityValues.estados.find((city) => city.sigla == state)?.cidades.map((cities, index) => (
+                                {CityValues.estados.find((city) => city.sigla === cadastro.state)?.cidades.map((cities, index) => (
                                     <option key ={index.toString()}  value = {cities}>{cities} </option>
                                 ))}
                             </Select>
@@ -64,7 +87,7 @@ export default function Registration() {
                     <div className='pt-12 flex space-x-10'>
                         <Button 
                             bg='bg-green65' 
-                            rounded='rounded-lg' 
+                            rounded='rounded' 
                             w='w-full' 
                             h='h-12' 
                             textColor='text-white' 
@@ -75,7 +98,7 @@ export default function Registration() {
                         </Button> 
                         <Button 
                             bg='bg-greenDark' 
-                            rounded='rounded-lg' 
+                            rounded='rounded' 
                             w='w-full' 
                             h='h-12' 
                             textColor='text-white' 
