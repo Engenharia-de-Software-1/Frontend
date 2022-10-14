6986419a753214b3fe6b4e7bc615be0cde12bf8c
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-10-13 21:41:12 -0300

    feat: startup registration page

--- src/pages/cadastroStartup/index.tsx
+++ src/pages/cadastroStartup/index.tsx
@@ -8,27 +8,17 @@ import { Stack } from '../../components/Stack';
 import { divGeneral, textTitle } from './styles';
 import axiosInstance from '../axiosInstance';
 
-interface ICadastroStartup{
-    startupName: string;
-    cnpj: string;
-    phone: string;
-    employees: number;
-    state: string;
-    city: string;
-}
-
-
-class IStartupUpdate {
-    phone?: string = '';
-    startupName?: string = '';
-    cnpj?: string = '';
-    employees?: number = 0;
-    city?: string = '';
-    state?: string = '';
+class ICadastroStartup{
+    startupName: string = '';
+    cnpj: string = '';
+    phone: string = '';
+    employees: number = 0;
+    state: string = '';
+    city: string = '';
 }
 
 export default function Registration() {    
-    const [cadastro,setCadastro] = useState<ICadastroStartup>({} as ICadastroStartup);
+    const [cadastro,setCadastro] = useState<ICadastroStartup>(new ICadastroStartup());
     
     const router = useRouter();
     const { userId } = router.query;
@@ -65,58 +55,58 @@ export default function Registration() {
                     <div className="w-36 h-8 bg-no-repeat bg-agroLogo "/>
 
                     <h1 className={textTitle}>Cadastro startup</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="w-462 mt-5">
+                            <Input haslabel label='Nome do representante' placeholder='Ex: José da Silva'/>
+                            <Input haslabel name='startupName' onChange={(e) => handleChange(e)} value={cadastro.startupName} label='Nome da startup' placeholder='Ex: Doe sangue' top='mt-10'/>
+                            <Input haslabel name='phone' onChange={(e) => handleChange(e)} value={cadastro.phone} label='Número de celular' placeholder='(00) 0 0000-0000' top='mt-10'/>
+                            <Input haslabel name='cnpj' onChange={(e) => handleChange(e)} value={cadastro.cnpj} label='CNPJ' placeholder='00000000000000' top='mt-10'/>
+                            <Input haslabel name='employees' onChange={(e) => handleChange(e)} value={cadastro.employees} label='Quantidade de pessoas na startup' placeholder='0' type='number' min='0' top='mt-10'/>                
 
-                    <div className="w-462 mt-5">
-                        <Input haslabel label='Nome do representante' placeholder='Ex: José da Silva'/>
-                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.startupName} label='Nome da startup' placeholder='Ex: Doe sangue' top='mt-10'/>
-                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.phone} label='Número de celular' placeholder='(00) 0 0000-0000' top='mt-10'/>
-                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.cnpj} label='CNPJ' placeholder='00000000000000' top='mt-10'/>
-                        <Input haslabel onChange={(e) => handleChange(e)} value={cadastro.employees} label='Quantidade de pessoas na startup' placeholder='0' type='number' min='0' top='mt-10'/>                
-
-                        <div className='flex space-x-10'>
-                            <Select onChange = {(e) => handleChange(e)} value = {cadastro.state} 
-                                haslabel label='Estado' top='mt-10' name='state'
-                                >
-                                <option key = 'init'>Selecione o Estado</option>
-                                {CityValues.estados.map((uf, index) => (
-                                    <option key ={index.toString()} value = {uf.sigla}>{uf.nome}</option>
-                                ))}
-                            </Select>
-                            <Select onChange = {(e) => handleChange(e)} value = {cadastro.city} 
-                                haslabel label='Cidade' top='mt-10'
-                                >
-                                <option key = 'init'>Selecione a cidade</option>
-                                {CityValues.estados.find((city) => city.sigla === cadastro.state)?.cidades.map((cities, index) => (
-                                    <option key ={index.toString()}  value = {cities}>{cities} </option>
-                                ))}
-                            </Select>
+                            <div className='flex space-x-10'>
+                                <Select name='state' onChange={(e) => handleChange(e)} value={cadastro.state} 
+                                    haslabel label='Estado' top='mt-10'
+                                    >
+                                    <option key = 'init'>Selecione o Estado</option>
+                                    {CityValues.estados.map((uf, index) => (
+                                        <option key ={index.toString()} value = {uf.sigla}>{uf.nome}</option>
+                                    ))}
+                                </Select>
+                                <Select name='city' onChange={(e) => handleChange(e)} value={cadastro.city} 
+                                    haslabel label='Cidade' top='mt-10'
+                                    >
+                                    <option key = 'init'>Selecione a cidade</option>
+                                    {CityValues.estados.find((city) => city.sigla === cadastro.state)?.cidades.map((cities, index) => (
+                                        <option key ={index.toString()}  value = {cities}>{cities} </option>
+                                    ))}
+                                </Select>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className='pt-12 flex space-x-10'>
-                        <Button 
-                            bg='bg-green65' 
-                            rounded='rounded' 
-                            w='w-full' 
-                            h='h-12' 
-                            textColor='text-white' 
-                            textWeight='font-bold'
-                            onClick={goBack}
-                            >
-                            VOLTAR
-                        </Button> 
-                        <Button 
-                            bg='bg-greenDark' 
-                            rounded='rounded' 
-                            w='w-full' 
-                            h='h-12' 
-                            textColor='text-white' 
-                            textWeight='font-bold'
-                            >
-                            FINALIZAR
-                        </Button> 
-
-                    </div> 
+                        <div className='pt-12 flex space-x-10'>
+                            <Button 
+                                bg='bg-green65' 
+                                rounded='rounded' 
+                                w='w-full' 
+                                h='h-12' 
+                                textColor='text-white' 
+                                textWeight='font-bold'
+                                onClick={goBack}
+                                >
+                                VOLTAR
+                            </Button> 
+                            <Button 
+                                bg='bg-greenDark' 
+                                rounded='rounded' 
+                                w='w-full' 
+                                h='h-12' 
+                                textColor='text-white' 
+                                textWeight='font-bold'
+                                >
+                                FINALIZAR
+                            </Button> 
+                        </div>
+                    </form>
                 </div>
             </div>  
         </Stack>
