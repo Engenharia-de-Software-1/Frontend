b7c455717b06ad96d5fcbd30289831d29ecff01e
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-19 13:27:27 -0300

    feat: profileInvestor api integration
    Co-authored-by: Yuri Baza <yuribaza2321@gmail.com>

--- src/pages/minhaContaInvestidor/index.tsx
+++ src/pages/minhaContaInvestidor/index.tsx
@@ -1,5 +1,5 @@
 import 'remixicon/fonts/remixicon.css';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Stack } from '../../components/Stack';
@@ -7,13 +7,24 @@ import Sidebar from '../../components/Sidebar';
 import { divGeneral, divInput, textStyle3, textTitle } from './styles';
 import { Select } from '../../components/Select';
 import CityValues from '../../contents/city';
+import api from '../../services/api';
+import { IUserInvestor } from '../../models/IUser';
 
-export default function ProfileAdmin() {    
-    const [_buttonEditMyAccount, setButtonEditMyAccount] = useState(false);
-    const [_buttonEditStartup, setButtonEditStartup] = useState(false);
-    const [_buttonEditAddress, setButtonEditAddress] = useState(false);
-    const [state, setState] = useState("");
-    const [city, setCity] = useState("");   
+export default function ProfileInvestor() {  
+    const userId = 'c552cfdd-97bd-4b58-b698-01cf656cc02a'; 
+
+    const [name, setName] = useState<string>('');
+    const [phone, setPhone] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    
+    const [companyName, setCompanyName] = useState<string>('');
+    const [cnpj, setCnpj] = useState<string>('');
+    const [profession, setProfession] = useState<string>('');
+    const [qtdMembers, setQtdMembers] = useState<number>(0);
+    
+    const [state, setState] = useState<string>('');
+    const [city, setCity] = useState<string>('');    
+    
 
     const handleUF = useCallback((state:string) => {
         setState(state)
@@ -23,21 +34,45 @@ export default function ProfileAdmin() {
         setCity(city)
     }, [])
 
-    function goEditMyAccount() {
-        setButtonEditMyAccount(true);
-        setButtonEditStartup(false);
-        setButtonEditAddress(false);
+    const getUserInfo = useCallback(async () => {
+        const response = await api.get<IUserInvestor>(`/investor/${userId}`);
+        setName(response.data.name);
+        setEmail(response.data.email);
+        setPhone(response.data.phone);
+        setCompanyName(response.data.investor.companyName);
+        setProfession(response.data.investor.profession);
+        setCnpj(response.data.investor.cnpj);
+        setQtdMembers(response.data.investor.qtdMembers);
+        setState(response.data.address.state);
+        setCity(response.data.address.city);
+    }, []);
+
+    async function handleEdit() {
+        try {
+            const output = await api.put<IUserInvestor>(`investor/${userId}`, {
+                name,
+                email,
+                phone,
+                companyName,
+                profession,
+                state,
+                city,
+                cnpj,
+                qtdMembers
+            });
+            if(output.data && output.data.id) {
+                alert('Dados atualizados com sucesso!');
+            } else {
+                alert('Erro ao atualizar dados!');
+            }
+        } catch (error) {
+            alert('Erro retornado!');
+        }
     }
-    function goEditStartup() {
-        setButtonEditMyAccount(false);
-        setButtonEditStartup(true);
-        setButtonEditAddress(false);
-    }
-    function goEditAddress() {
-        setButtonEditMyAccount(false);
-        setButtonEditStartup(false);
-        setButtonEditAddress(true);
-    }
+
+    useEffect(() => {
+        getUserInfo();
+    }, [getUserInfo]);
 
     return ( 
         <Stack bg='bg-white'>
@@ -49,10 +84,31 @@ export default function ProfileAdmin() {
                     <h1 className={textTitle}>Minha conta</h1> 
 
                     <div className={divInput}>                        
-                        <Input haslabel label='Nome' placeholder='nome completo' top='mt-5'/>
-                        <Input haslabel label='Telefone celular' placeholder='(00) 0 0000-0000)' top='mt-5'/>
+                        <Input 
+                            haslabel 
+                            label='Nome' 
+                            placeholder='nome completo' 
+                            top='mt-5'
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        <Input 
+                            haslabel 
+                            label='Telefone celular' 
+                            placeholder='(00) 0 0000-0000)' 
+                            top='mt-5'
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
                     </div>
-                        <Input haslabel label='E-mail' placeholder='e-mail' top='mt-10'/>
+                        <Input 
+                            haslabel 
+                            label='E-mail' 
+                            placeholder='e-mail' 
+                            top='mt-10'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
 
                     <div className='pt-12'>
                         <Button 
@@ -62,8 +118,8 @@ export default function ProfileAdmin() {
                             h='h-12' 
                             textColor='text-white' 
                             textWeight='font-bold'
-                            onClick={goEditMyAccount}
-                            >
+                            onClick={handleEdit}
+                        >
                             EDITAR
                         </Button>
                     </div> 
@@ -71,13 +127,43 @@ export default function ProfileAdmin() {
                     <h1 className={textStyle3}>Dados do investidor</h1>
                     
                     <div className={divInput}>
-                        <Input haslabel label='Nome da startup ou equipe' placeholder='nome' top='mt-5'/>
-                        <Input haslabel label='CNPJ' placeholder='00000000000000' top='mt-5'/>                            
+                        <Input 
+                            haslabel 
+                            label='Nome da startup ou equipe' 
+                            placeholder='nome' 
+                            top='mt-5'
+                            value={companyName}
+                            onChange={(e) => setCompanyName(e.target.value)}
+                        />
+                        <Input 
+                            haslabel 
+                            label='CNPJ' 
+                            placeholder='00000000000000' 
+                            top='mt-5'
+                            value={cnpj}
+                            onChange={(e) => setCnpj(e.target.value)}
+                        />                            
                     </div>
 
                     <div className={divInput}>
-                        <Input haslabel label='Quantidade de membros/sócios' placeholder='0' type="number" min='0' top='mt-10'/>
-                        <Input haslabel label='Formação em quais áreas (membros/sócios)' placeholder='formações' top='mt-10'/>                            
+                        <Input 
+                            haslabel 
+                            label='Quantidade de membros/sócios' 
+                            placeholder='0' 
+                            type="number" 
+                            min='0' 
+                            top='mt-10'
+                            value={qtdMembers}
+                            onChange={(e) => setQtdMembers(parseInt(e.target.value))}
+                       />
+                        <Input 
+                            haslabel 
+                            label='Formação em quais áreas (membros/sócios)' 
+                            placeholder='formações' 
+                            top='mt-10'
+                            value={profession}
+                            onChange={(e) => setProfession(e.target.value)}
+                        />                            
                     </div>
                     
                     <div className='pt-12'>
@@ -88,7 +174,7 @@ export default function ProfileAdmin() {
                             h='h-12' 
                             textColor='text-white' 
                             textWeight='font-bold'
-                            onClick={goEditStartup}
+                            onClick={handleEdit}
                             >
                             EDITAR
                         </Button>
@@ -102,14 +188,14 @@ export default function ProfileAdmin() {
                             >
                             <option key = 'init'>Selecione o Estado</option>
                             {CityValues.estados.map((uf, index) => (
-                                <option key ={index.toString()} value = {uf.sigla}>{uf.nome}</option>
+                                <option key ={index.toString()} value = {uf.nome}>{uf.nome}</option>
                             ))}
                         </Select>
                         <Select onChange = {(e) => handleCity(e.target.value)} value = {city} 
                             haslabel label='Cidade' top='mt-5'
                             >
                             <option key = 'init'>Selecione a cidade</option>
-                            {CityValues.estados.find((city) => city.sigla == state)?.cidades.map((cities, index) => (
+                            {CityValues.estados.find((city) => city.nome == state)?.cidades.map((cities, index) => (
                                 <option key ={index.toString()}  value = {cities}>{cities} </option>
                             ))}
                         </Select>
@@ -123,8 +209,8 @@ export default function ProfileAdmin() {
                             h='h-12' 
                             textColor='text-white' 
                             textWeight='font-bold'
-                            onClick={goEditAddress}
-                            >
+                            onClick={handleEdit}
+                        >
                             EDITAR 
                         </Button>
                     </div> 
