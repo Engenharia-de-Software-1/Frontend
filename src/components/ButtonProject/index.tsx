ae63806947c5f9287abec7944280926c103519c3
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-10-30 21:30:22 -0300

    feat: putting the star to favorite

--- src/components/ButtonProject/index.tsx
+++ src/components/ButtonProject/index.tsx
@@ -1,5 +1,5 @@
 import 'remixicon/fonts/remixicon.css';
-import React from "react";
+import React, { useState } from "react";
 import { format } from 'date-fns';
 import { IProject } from "../../models/IProject";
 
@@ -7,23 +7,31 @@ interface IProjectProps {
     project: IProject;
 }
 
-
 function ButtonProject({ project }: IProjectProps) {   
-   
+   const [starButton, setStarButton] = useState(false);
+
+    function useButtonStar() {
+        setStarButton(!starButton);
+}
     return (          
-        <button className='bg-transparent text-black'>
+        <button className='relative bg-transparent text-black'>
             
             <div className='flex justify-between w-full '>
                 <h1 className="text-start text-2xl font-semibold pt-8">
                     {project.nameProject}
                 </h1>
 
-                <button icon="ri-start-line" className='w-12 h-12 bg-black'/>
+                <div className='absolute bottom-[170px] top-0 left-full'>
+                    <button onClick={useButtonStar}  className='ri-star-line ri-2x'/>
+                    {/* console.log(starButton) */}
+                    <button onClick={useButtonStar} className='ri-star-fill ri-2x'/>
+                </div>
             </div>
               
             <h1 className="text-start text-justify pt-5">
                 {project.description}    
             </h1>     
+
             <h2 className='text-black62 text-sm text-right pb-5'> 
                 Criado em {format(project.created_at, "dd/mm/yyyy")}
             </h2>
--- src/pages/meusProjetosStartup/index.tsx
+++ src/pages/meusProjetosStartup/index.tsx
@@ -1,4 +1,3 @@
-import 'remixicon/fonts/remixicon.css';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Stack } from '../../components/Stack';
 import Sidebar from '../../components/Sidebar';
