4e96852da4cfdaf3a5be1b2ee0d2a08cc0fb6414
GabrielaMarangoni <marangonigabriela@gmail.com>
2022-11-03 17:10:28 -0300

    feat: screen pending projects

--- src/components/ButtonProject/index.tsx
+++ src/components/ButtonProject/index.tsx
@@ -2,6 +2,7 @@ import 'remixicon/fonts/remixicon.css';
 import React, { useState } from "react";
 import { format } from 'date-fns';
 import { IProject } from "../../models/IProject";
+import { Button } from '../Button';
 
 interface IProjectProps {
     project: IProject;
@@ -10,11 +11,14 @@ interface IProjectProps {
 function ButtonProject({ project }: IProjectProps) {   
    const [starButton, setStarButton] = useState(false);
 
+   const [userType, setUserType] = useState('admin');
+
     function useButtonStar() {
         setStarButton(!starButton);
     }
 
-    return (          
+    return (   
+        <>
         <button className='bg-transparent text-black'>
             
             <div className='rel flex justify-between mt-8 w-full '>
@@ -22,19 +26,66 @@ function ButtonProject({ project }: IProjectProps) {
                     {project.nameProject}
                 </h1>
 
-                <div className='abs'>
-                    <button onClick={useButtonStar} className={`${starButton ? 'ri-star-fill text-orange-300' : 'ri-star-line text-stone-700'} ri-2x`}/>
-                </div>
+                {userType !== 'admin' && (
+                    <div className='abs'>
+                        <button onClick={useButtonStar} className={`${starButton ? 'ri-star-fill text-orange-300' : 'ri-star-line text-stone-700'} ri-2x`}/>
+                    </div>                  
+                
+            )}   
             </div>
-              
-            <h1 className="text-justify pt-5">
-                {project.description}    
-            </h1>     
+            {userType !== 'admin' && (
+                <h1 className="text-justify pt-5">
+                    {project.description}    
+                </h1>              
+                
+            )} 
+           
+            {userType === 'admin' && (
+                <>
+                    <h1 className="text-justify pt-5">
+                        <b>Solução: </b> {project.description}    
+                    </h1>              
+                    <h1 className="text-justify pt-5">
+                        <b>Problema: </b> {project.problem}    
+                    </h1>   
+                </>
+                
+            )}  
+
 
             <h2 className='text-black62 text-sm text-right pb-5'> 
                 Criado em {format(project.created_at, "dd/mm/yyyy")}
             </h2>
+
+            {userType === 'admin' && (
+                <div className='pb-5 text-right space-x-5'>
+                    <Button
+                        bg='bg-greenDark' 
+                        rounded='rounded-lg' 
+                        w='w-36' 
+                        h='h-12' 
+                        textColor='text-white' 
+                        textWeight='font-bold'
+                    >
+                        APROVAR
+                    </Button>
+
+                    <Button
+                        bg='bg-warning' 
+                        rounded='rounded-lg' 
+                        w='w-36' 
+                        h='h-12' 
+                        textColor='text-white' 
+                        textWeight='font-bold'
+                    >
+                        RECUSAR
+                    </Button>
+                </div>                       
+                
+            )}  
         </button>       
+        
+        </>    
     );
 }
 
