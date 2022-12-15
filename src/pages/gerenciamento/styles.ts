17e6ed050fb99b13006a774e2ebea8a4ad534b33
getuliobr <getulioregis@alunos.utfpr.edu.br>
2022-12-15 00:01:11 -0300

    feat: edit startup

--- src/pages/gerenciamento/styles.ts
+++ src/pages/gerenciamento/styles.ts
@@ -1,3 +1,6 @@
 export const textTitle = 'font-semibold text-4xl';
 export const divGeneral = 'flex flex-col ml-80 p-32 bg-white h-screen w-full pt-24';
 export const textStyle = 'text-2xl font-semibold mt-5 pb-3';
+export const textStyle2 = 'font-semibold text-2xl pb-5';
+export const textStyle3 = 'font-semibold text-2xl pt-5';
+export const divInput = 'flex items-center w-full space-x-5';
